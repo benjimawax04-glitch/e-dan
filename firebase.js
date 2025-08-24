2652d84e3d9876873a8d16d1d5957401d67de241
PyrexCarter <mbaracarter@gmail.com>
2025-08-24 23:03:42 +0100

    les néo-2

--- firebase.js
+++ firebase.js
@@ -1,13 +1,23 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_AUTH_DOMAIN",
+  apiKey: "TA_CLE_API",
+  authDomain: "TON_DOMAINE.firebaseapp.com",
   projectId: "TON_PROJECT_ID",
+  storageBucket: "TON_BUCKET.appspot.com",
+  messagingSenderId: "TON_SENDER_ID",
+  appId: "TON_APP_ID"
 };
 
+// Initialise Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const sessionsCollection = collection(db, "sessions");
+
+// Initialise Firestore
+const db = getFirestore(app);
+
+// Référence à la collection "sessions"
+const sessionsCollection = collection(db, 'sessions');
+
+export { db, sessionsCollection };
